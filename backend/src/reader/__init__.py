46c424f041c3d6a2686d7b065fdbbf95671d7c4e
dmitriy <dmitriyvasil@gmail.com>
2026-05-17 11:16:36 +0200

    chore: bump version to 0.1.1

--- backend/src/reader/__init__.py
+++ backend/src/reader/__init__.py
@@ -5,7 +5,7 @@ __all__ = ("__version__", "appbuilder", "db", "migrate")
 from flask_appbuilder import AppBuilder, SQLA
 from flask_migrate import Migrate
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 appbuilder = AppBuilder()
 migrate = Migrate()
