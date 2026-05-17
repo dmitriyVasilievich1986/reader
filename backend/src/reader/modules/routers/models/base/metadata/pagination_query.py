ea4b19909454fb1efe1bbe4a51ce1bc31735a0f1
dmitriy <dmitriyvasil@gmail.com>
2026-05-17 22:52:54 +0200

    refactor: Update pagination_query limit description for clarity
    - Modified the description of the limit field in the PaginationQuery class to specify the minimum and maximum values directly in the text, enhancing clarity for users regarding pagination constraints.

--- backend/src/reader/modules/routers/models/base/metadata/pagination_query.py
+++ backend/src/reader/modules/routers/models/base/metadata/pagination_query.py
@@ -12,7 +12,7 @@ from ..query import BaseQueryModel
 class PaginationQuery[SortByType: str](BaseQueryModel):
     """Query parameters for pagination."""
 
-    limit: int = Field(default=100, ge=1, le=100, description="The number of items to return")
+    limit: int = Field(default=100, ge=1, le=100, description="The number of items to return(min 1, max 100)")
     offset: int = Field(default=0, ge=0, description="The number of items to skip")
     sort_order: Literal["asc", "desc"] = Field(default="asc", description="The order to sort the items by")
     sort_by: SortByType = Field(default="id", description="The field to sort the items by")  # type: ignore[assignment]
