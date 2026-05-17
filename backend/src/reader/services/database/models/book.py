bdc6bc9f7004f29d96b3885d6c434fc9363172c1
dmitriy <dmitriyvasil@gmail.com>
2026-05-17 20:41:48 +0200

    feat: Enhance relationships in database models
    - Updated the Author, Book, Category, and Page models to include back_populates for relationships, improving bidirectional access between related entities.
    - This change enhances the clarity and usability of the ORM mappings, facilitating easier data retrieval and manipulation across related models.

--- backend/src/reader/services/database/models/book.py
+++ backend/src/reader/services/database/models/book.py
@@ -26,10 +26,10 @@ class Book(Base):
     cover: Mapped[str | None] = mapped_column(String, nullable=True)
 
     author_id: Mapped[int] = mapped_column(Integer, ForeignKey("author.id"))
-    author: Mapped["Author"] = relationship("Author")
+    author: Mapped["Author"] = relationship("Author", back_populates="books")
 
     categories: Mapped[list["Category"]] = relationship("Category", secondary="category_book_table")
-    pages: Mapped[list["Page"]] = relationship("Page")
+    pages: Mapped[list["Page"]] = relationship("Page", back_populates="book")
 
     @property
     def author_name(self) -> str:
