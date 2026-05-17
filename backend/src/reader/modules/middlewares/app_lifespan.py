d55e3e0bc8f76fa9e84ea5d156e3d1517d80ce5d
dmitriy <dmitriyvasil@gmail.com>
2026-05-17 15:28:51 +0200

    feat: Add modules for Reader application
    - Introduced new `__init__.py` files for the main modules and middlewares, providing structure and documentation for the Reader application.
    - Implemented `app_lifespan.py` to manage the application lifecycle in FastAPI, including service initialization and cleanup, enhancing application stability and resource management.

---
+++ backend/src/reader/modules/middlewares/app_lifespan.py
@@ -0,0 +1,45 @@
+"""Application lifespan management for FastAPI."""
+
+__all__ = ("lifespan",)
+
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from loguru import logger
+
+from reader.config.app_config import AppConfig
+from reader.services.database import AsyncDatabaseClient
+
+
+@asynccontextmanager
+async def lifespan(
+    app: FastAPI,
+):
+    """Manage the application lifecycle, initializing and cleaning up services.
+
+    Args:
+        app: The FastAPI application instance.
+
+    Yields:
+        None: Control returns to the application during its lifetime.
+
+    Raises:
+        ConnectionError: If unable to connect to services.
+        Exception: If services fail to initialize.
+
+    """
+    logger.info("Starting application lifespan...")
+    logger.info(f"Debug mode: {app.debug}")
+
+    # Initialize services
+    logger.info("Initializing Database client...")
+    app_config = AppConfig.get_or_create()
+    database_client = AsyncDatabaseClient(app_config=app_config)
+    logger.info("Database client initialized successfully.")
+
+    yield
+
+    # Cleanup
+    logger.info("Shutting down application lifespan...")
+    await database_client.close()
+    logger.info("Database client closed successfully.")
