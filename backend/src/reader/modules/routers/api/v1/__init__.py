472b1044ab7b69adbb0fdffaa88d6d71400b25f3
dmitriy <dmitriyvasil@gmail.com>
2026-05-18 14:10:52 +0200

    feat: Implement user management API routes
    - Added a new user router with endpoints for user login, retrieving the current user, and updating user profiles.
    - Introduced request and response models for user login and profile updates, enhancing the API's structure and usability.
    - Integrated user authentication and authorization mechanisms using JWT, improving security for user-related operations.

--- backend/src/reader/modules/routers/api/v1/__init__.py
+++ backend/src/reader/modules/routers/api/v1/__init__.py
@@ -8,6 +8,7 @@ from .authors import router as authors_router
 from .books import router as books_router
 from .categories import router as categories_router
 from .pages import router as pages_router
+from .user import router as user_router
 
 router = APIRouter(prefix="/v1")
 
@@ -15,3 +16,4 @@ router.include_router(authors_router)
 router.include_router(books_router)
 router.include_router(categories_router)
 router.include_router(pages_router)
+router.include_router(user_router)
---
+++ backend/src/reader/modules/routers/models/responses/user/get_single_user.py
@@ -0,0 +1,19 @@
+"""Get single user response model."""
+
+__all__ = ("GetSingleUserResponse",)
+
+from pydantic import Field
+
+from reader.modules.routers.models.base import BaseResponseFromModelSchema
+
+
+class GetSingleUserResponse(BaseResponseFromModelSchema):
+    """Response model for getting a single user."""
+
+    id: int = Field(..., description="The ID of the user")
+    username: str = Field(..., description="The username of the user")
+    email: str = Field(..., description="The email of the user")
+    first_name: str | None = Field(None, description="The first name of the user")
+    last_name: str | None = Field(None, description="The last name of the user")
+    photo_url: str | None = Field(None, description="The photo URL of the user")
+    is_active: bool = Field(..., description="Whether the user is active")
