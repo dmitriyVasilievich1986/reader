0c915b3fcb7480d0d1dc697428cbde66407a0fea
dmitriy <dmitriyvasil@gmail.com>
2026-05-18 10:14:07 +0200

    feat: Enhance module resolution in frontend configuration
    - Added baseUrl and paths in tsconfig.app.json to simplify module imports for components, pages, and services.
    - Updated vite.config.ts to include alias resolution for the same modules, improving code organization and import clarity.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": { mode: "produciton" },
+  },
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "./src/components"),
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@services": resolve(__dirname, "./src/services"),
+    },
   },
   build: {
     outDir: "../static/js",
