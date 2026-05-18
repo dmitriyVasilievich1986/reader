45fd284030c840009d94c425dec7e23f186a3015
dmitriy <dmitriyvasil@gmail.com>
2026-05-18 20:28:09 +0200

    refactor: Remove PagesPage export from barrel module
    - Updated the barrel module in `index.tsx` to remove the `PagesPage` export, streamlining the module and improving clarity in component organization.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,9 @@
 /**
- * Barrel module for the pages: re-exports {@link BooksPage}, {@link SinlgeBookPage}, {@link ReadBookPage}, {@link HomePage}, {@link Login}, {@link PagesPage}.
+ * Barrel module for the pages: re-exports {@link BooksPage}, {@link SinlgeBookPage}, {@link ReadBookPage}, {@link HomePage}, {@link Login}.
  */
 
 import { SinlgeBookPage, BooksPage, ReadBookPage } from './books';
 import { HomePage } from './home/HomePage';
 import { Login } from './login/Login';
-import { PagesPage } from './pages/PagesPage';
 
-export { BooksPage, SinlgeBookPage, ReadBookPage, HomePage, Login, PagesPage };
+export { BooksPage, SinlgeBookPage, ReadBookPage, HomePage, Login };
