bc6e25ca1daf91829fccb44ce56320b143665032
dmitriy <dmitriyvasil@gmail.com>
2026-05-19 18:53:25 +0200

    feat: Enhance SingleBookPage with watch count and navigation improvements
    - Added a visibility icon and watch count display to the SingleBookPage, providing users with engagement metrics for each book.
    - Implemented a new `readBookClickHandler` function to increment the watch count and navigate to the reading page, improving user interaction and tracking.

--- frontend/src/pages/books/singleBookPage/SingleBookPage.tsx
+++ frontend/src/pages/books/singleBookPage/SingleBookPage.tsx
@@ -4,6 +4,7 @@
  * @module pages/books/singleBookPage/SingleBookPage
  */
 
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -30,7 +31,7 @@ export function SingleBookPage() {
 
   const [book, setBook] = useState<BookType | null>(null);
 
-  const { getBookBySlug } = useBookAPIClient();
+  const { getBookBySlug, incrementWatchesCount } = useBookAPIClient();
 
   useEffect(() => {
     if (!bookId) return;
@@ -41,6 +42,13 @@ export function SingleBookPage() {
       setBook(null);
     };
   }, [bookId]);
+
+  const readBookClickHandler = () => {
+    if (!book) return;
+
+    incrementWatchesCount(book.id);
+    navigate(`/book/${book.id}/read`);
+  };
 
   const authorName = useMemo(() => {
     if (!book) return '';
@@ -74,10 +82,16 @@ export function SingleBookPage() {
             <Typography className={styles.author} variant="body1">
               {authorName}
             </Typography>
+            <Box className={styles.watchesCountContainer}>
+              <VisibilityIcon />
+              <Typography className={styles.watchesCount} variant="body1">
+                {book.watchesCount} views
+              </Typography>
+            </Box>
             <Button
               className={styles.readButton}
               variant="contained"
-              onClick={() => navigate(`/book/${bookId}/read`)}
+              onClick={readBookClickHandler}
             >
               Read
             </Button>
